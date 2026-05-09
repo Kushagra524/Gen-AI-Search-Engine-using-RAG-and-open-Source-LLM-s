63f587568c5221e58f6caad80975e6dd792c6c0e
Kushagra Srivastava <kushsrivastava524@gmail.com>
2026-05-09 16:40:25 +0530

    deprecations removed

--- end_to_end_search_engine.py
+++ end_to_end_search_engine.py
@@ -5,7 +5,7 @@ from langchain_groq import ChatGroq
 from langchain_community.utilities import ArxivAPIWrapper , WikipediaAPIWrapper
 from langchain_community.tools import ArxivQueryRun , DuckDuckGoSearchRun, WikipediaQueryRun
 from langchain.agents import initialize_agent , AgentType
-from langchain.callbacks import StreamlitCallbackHandler
+from langchain_community.callbacks import StreamlitCallbackHandler
 import os
 from dotenv import load_dotenv
 
